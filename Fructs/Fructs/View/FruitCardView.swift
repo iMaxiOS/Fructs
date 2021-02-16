317a080a0cd5bc2605d2b82ad2650ab2e86435f8
Macostik <y.granchenko@gmail.com>
2021-02-16 13:56:02 +0200

    add colors, images, folders

---
+++ Fructs/Fructs/View/FruitCardView.swift
@@ -0,0 +1,21 @@
+//
+//  FruitCardView.swift
+//  Fructs
+//
+//  Created by Maxim Granchenko on 16.02.2021.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .pre
+    }
+}
