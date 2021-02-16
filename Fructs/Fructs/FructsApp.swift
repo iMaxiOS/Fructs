ca498efc200b68879a23239c6f74b1fb1a32d98e
Macostik <y.granchenko@gmail.com>
2021-02-16 13:13:52 +0200

    create app

---
+++ Fructs/Fructs/FructsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructsApp.swift
+//  Fructs
+//
+//  Created by Maxim Granchenko on 16.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct FructsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
